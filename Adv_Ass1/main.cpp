bac48b33d8d7602b467e836f7329d68541e0f43c
刘铮 <liuzheng20@gmail.com>
2018-01-25 17:30:04 -0500

    Initial Commit

---
+++ Adv_Ass1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Adv_Ass1
+//
+//  Created by zheng liu on 1/25/18.
+//  Copyright © 2018 zheng liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
